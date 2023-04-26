16e316bbe0e7d47384e7b1473bfd6fa8da561a66
Manh Le <manhlv@gostream.vn>
2023-04-26 16:57:42 +0700

    update env

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -5,7 +5,7 @@ const { cookies } = useCookies();
 
 
 const axiosApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3100",
 });
 
 enum MessageError {
